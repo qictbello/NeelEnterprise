7fa6fe16d15dee2c887251dd1f1ca5f36b881fd8
qictbello <112616466+qictbello@users.noreply.github.com>
2023-12-13 21:16:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,37 +7,42 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-//configure env
+// Configure env
 dotenv.config();
 
-//database config
+// Database config
 connectDB();
 
-//rest object
+// Get the current file's path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Rest object
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
+// Rest API
 app.use("*", function(req, res){
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Run listen
 app.listen(PORT, () => {
   console.log(
     `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
